a1d2b47453ba4ada98737533d23aab31f4b3d0d9
Shu12388y <spl.sp999@gmail.com>
2023-08-02 18:37:34 +0530

    file are added

---
+++ helloWorld.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+echo "hello world !"
+
+#we can also use bash command in here 
+
+ls
+# list of files and folders in the dir.
+
+pwd
+# present working dir.
+
+npm init -y
+# we can also write command that we want to execute
