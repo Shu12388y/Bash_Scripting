c90e3e75889465b16993d73e948ed8f5a6aba860
Shu12388y <spl.sp999@gmail.com>
2023-08-04 22:08:52 +0530

    done

--- python_install_script.sh
+++ python_install_script.sh
@@ -1,11 +1,19 @@
 # sudo apt install python3 -y
 # sudo apt install pip -y
 # sudo apt update && sudo apt upgrade
-file="python3"
-if command -v  $file
-then
-    echo "done ....."
-else
-    echo "error !!!!"
+# file="python3"
+# if command -v  $file
+# then
+#     echo "done ....."
+# else
+#     echo "error !!!!"
 
-fi
+# fi
+
+
+if [ -f /usr/bin/python3 ]
+then
+    echo "file exit....."
+else
+    echo "error !! "    
+fi    
